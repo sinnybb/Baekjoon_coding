8b6189c270fe3f02e58cb1129ff44585f3356eb0
sinnybb <153700515+sinnybb@users.noreply.github.com>
2024-08-16 19:24:03 +0900

    [Bronze V] Title: A+B - 3, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -0,0 +1,6 @@
+# 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+# 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+n = int(input())
+for i in range(1, n+1):
+    a, b = map(int, input().split())
+    print(a + b)
