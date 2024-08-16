20be92b80dd76f0c9c6037c4cce561af9b7b39ba
sinnybb <153700515+sinnybb@users.noreply.github.com>
2024-08-16 22:51:22 +0900

    [Bronze V] Title: A+B - 5, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -0,0 +1,13 @@
+# 입력은 여러 개의 테스트 케이스로 이루어져 있다. 
+# 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+# 입력의 마지막에는 0 두 개가 들어온다.
+import sys
+
+data = sys.stdin.read().splitlines()
+
+for line in data:
+    a, b = map(int, line.split())
+    # 0,0 처리
+    if a == 0 and b == 0 :
+        break
+    print(a + b)
