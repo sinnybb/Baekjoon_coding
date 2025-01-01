63c82348e679f5e2b2ead03452eec0febdab32c9
sinnybb <153700515+sinnybb@users.noreply.github.com>
2025-01-02 00:16:25 +0900

    [Silver III] Title: 소수 구하기, Time: 292 ms, Memory: 42212 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,15 @@
+import math
+import sys
+
+m, n = map(int, sys.stdin.readline().split())
+prime = [True] * (n + 1)  
+prime[0] = prime[1] = False  
+
+for i in range(2, int(math.sqrt(n)) + 1):
+    if prime[i]: 
+        for j in range(i * i, n + 1, i):
+            prime[j] = False
+
+for i in range(m, n + 1):
+    if prime[i]:
+        print(i)
