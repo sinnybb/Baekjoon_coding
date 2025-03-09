4fcfd41fc82c3ccced632d334471b1cafe93d792
sinnybb <153700515+sinnybb@users.noreply.github.com>
2025-03-09 15:59:58 +0900

    [Silver III] Title: 1로 만들기, Time: 468 ms, Memory: 40224 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+num = int(input())
+
+dp = [0] * 1000001
+
+for i in range(2, num + 1):
+    dp[i] = dp[i-1] +1
+
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+    
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+        
+
+print(dp[num])
